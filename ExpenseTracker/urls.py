0cf86c098720bd125be94a3a7c267dd28b40cf7b
YacineHam <yacinehamitou32@gmail.com>
2023-12-12 13:18:14 +0100

    Update urls and settings File

--- ExpenseTracker/urls.py
+++ ExpenseTracker/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from receipts.views import ReceiptListView,ReceiptDetailView,ReceiptCreateView,ReceiptUpdateView,ReceiptDeleteView
+from Receipts.views import ReceiptListView,ReceiptDetailView,ReceiptCreateView,ReceiptUpdateView,ReceiptDeleteView,SignUpView
+from django.contrib.auth.views import LoginView, LogoutView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', SignUpView.as_view(), name='register'),
     path('', ReceiptListView.as_view(), name='receipt-list'),
     path('receipt/<int:pk>/', ReceiptDetailView.as_view(), name='receipt-detail'),
     path('receipt/new/', ReceiptCreateView.as_view(), name='receipt-create'),
